d5190b6f21eb06bbf4588a2f15bda0121f805397
Bhaikko <siddhartha.pawar17@gmail.com>
2019-12-02 22:59:27 +0530

    Better UI for dating app

---
+++ DatingApp-SPA/src/app/_resolvers/member-detail.resolver.ts
@@ -0,0 +1,28 @@
+import { Injectable } from "@angular/core";
+import { Resolve, Router, ActivatedRouteSnapshot } from '@angular/router';
+import { User } from 'src/_models/User';
+import { UserService } from '../_services/user.service';
+import { AlertifyService } from '../_services/alertify.service';
+import { catchError } from 'rxjs/operators';
+import { Observable, of } from 'rxjs';
+
+
+// Resolvers are used to get data by side effect before the route is loaded
+@Injectable()
+export class MemberDetailResolver implements Resolve<User> {
+
+    constructor (private userService: UserService, private router: Router, private alertify: AlertifyService) {
+
+    }
+
+    resolve(route: ActivatedRouteSnapshot): Observable<User> {
+        return this.userService.getUser(route.params['id']).pipe(
+            catchError(error => {
+                this.alertify.error("Problem Retriving Data");
+                this.router.navigate(['/members']);
+                return of(null);    // to return observable with null
+            })
+        );
+    }
+    
+}
--- DatingApp-SPA/src/app/home/home.component.ts
+++ DatingApp-SPA/src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -11,7 +10,7 @@ export class HomeComponent implements OnInit {
   registerMode = false;
   values: any;
 
-  constructor(private http: HttpClient) { }
+  constructor() { }
 
   ngOnInit() {
   }

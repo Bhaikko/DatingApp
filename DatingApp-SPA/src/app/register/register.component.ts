e5dbcd8aef1dc111107c7c2934bd4a79c263c712
Bhaikko <siddhartha.pawar17@gmail.com>
2019-12-09 21:55:14 +0530

    Register form extended

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
 import { AlertifyService } from '../_services/alertify.service';
+import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from '../_models/User';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -9,22 +13,64 @@ import { AlertifyService } from '../_services/alertify.service';
 })
 export class RegisterComponent implements OnInit {
 
-  model: any = {};
+  user: User;
+  registerForm: FormGroup;
+  bsConfig: Partial<BsDatepickerConfig>;
 
   @Output() cancelRegister = new EventEmitter<boolean>();
 
-  constructor(private authService: AuthService, private alertify: AlertifyService) { }
+  constructor(private authService: AuthService, private alertify: AlertifyService, private fb: FormBuilder, private router: Router) { }
 
   ngOnInit() {
+    this.bsConfig = {
+      containerClass: 'theme-red'
+    }
+    this.createRegisterForm();
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {
+      'mismatch': true 
+    };
+  }
+
+  createRegisterForm () {
+    // Form Builder is use as syntax sugar for FormGroup and FormControl
+    this.registerForm = this.fb.group({
+      gender: ['male'],
+      username: ['', Validators.required],
+      knownAs: ['', Validators.required],
+      dateOfBirth: [null, Validators.required],
+      city: ['', Validators.required],
+      country: ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required]]
+    }, {
+      validators: this.passwordMatchValidator
+    });
   }
 
   register () {
-    this.authService.register(this.model)
-      .subscribe(() => {
-        this.alertify.success("Registration Successful");
-      }, err => {
-        this.alertify.error(err);
-      });
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      this.authService.register(this.user)
+        .subscribe(() => {
+          this.alertify.success("Registration Successfull")
+        }, err => {
+          this.alertify.error(err);
+        }, () => {
+          this.authService.login(this.user)
+            .subscribe(() => {
+              this.router.navigate(['/members'])
+            })
+        });
+    }
+    // this.authService.register(this.model)
+    //   .subscribe(() => {
+    //     this.alertify.success("Registration Successful");
+    //   }, err => {
+    //     this.alertify.error(err);
+    //   });
   }
 
   cancel () {

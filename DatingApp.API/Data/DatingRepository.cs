5963fb47ff286b7145a5d8bb14370bfd42a67850
Bhaikko <siddhartha.pawar17@gmail.com>
2019-12-10 21:19:41 +0530

    Paging added

--- DatingApp.API/Data/DatingRepository.cs
+++ DatingApp.API/Data/DatingRepository.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using DatingApp.API.Models;
 using Microsoft.EntityFrameworkCore;
+using DatingApp.API.Helpers;
 
 namespace DatingApp.API.Data
 {
@@ -32,11 +33,11 @@ namespace DatingApp.API.Data
             return user;
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<PagedList<User>> GetUsers(UserParams userParams)
         {
-            var users = await this._context.Users.Include(p => p.Photos).ToListAsync();
+            var users = this._context.Users.Include(p => p.Photos);
 
-            return users;
+            return await PagedList<User>.CreateAsync(users, userParams.PageNumber, userParams.PageSize);
         }
 
         public async Task<bool> SaveAll()
